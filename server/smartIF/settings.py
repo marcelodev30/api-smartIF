da30064ad38bacb0cfeb43b308bc1d5536e8caf6
Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-10-06 17:01:11 -0300

    add rest_framework

--- server/smartIF/settings.py
+++ server/smartIF/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'modulosSmart'
+    'modulosSmart',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
