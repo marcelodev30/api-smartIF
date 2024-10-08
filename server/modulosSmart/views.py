9f5a9555e25bef4202249b35abeebda0ac9c872a
Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-10-08 00:23:02 -0300

    add views

--- server/modulosSmart/views.py
+++ server/modulosSmart/views.py
@@ -1,12 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.db.models import Q
 from .models import Comando as dbComando
 
 class DispositivoSendMQTT(APIView):
     def post(self, request):
-        id = request.data.get('id')
-        Comando = request.data.get('Comando')
-        dados = dbComando.objects.filter(nome=Comando,dispositivo=id).values_list('codigo',flat=True)
-        return Response({'status': 'success', 'Comando': f'{str(dados).split("'")[1]}'}, status=status.HTTP_200_OK)
+
+        request_id = request.data.get('id')
+        request_Comando = request.data.get('Comando')
+        query_dados = dbComando.objects.filter(nome = request_Comando, dispositivo = request_id)
+
+        if(query_dados.exists()):
+            dado = str(query_dados.values_list('codigo',flat=True)).split("'")[1]
+            return Response({'status': 'success', 'Comando': f'{dado}'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'status': 'error'}, status=status.HTTP_404_NOT_FOUND)
