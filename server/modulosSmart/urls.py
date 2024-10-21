b961ea024920c9b3be836a27be7d7671792acdaf
Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-10-21 00:17:34 -0300

    add token

--- server/modulosSmart/urls.py
+++ server/modulosSmart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import DispositivoSendMQTT,GetDispositivos
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('api/Dispositivos/send', DispositivoSendMQTT.as_view(), name='send'),
     path('api/Dispositivos', GetDispositivos.as_view(), name='getData'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
