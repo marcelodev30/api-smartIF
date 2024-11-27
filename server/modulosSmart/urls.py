bf84cc2fa5df92cf400b080e28bc951aa6a1ebe2
Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-11-26 23:01:46 -0300

    ......

--- server/modulosSmart/urls.py
+++ server/modulosSmart/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('api/usuarios/', UsuárioViews.as_view(), name='user'),
     path('api/usuario/<int:idKey>/', UsuárioDetalhesViews.as_view(), name='usuario-detalhes'),
     path('api/usuarios/nivel-acesso/<uuid:idKey>', UsuárioNívelAcessoViews.as_view(), name='usuario_nivel'),
-    path('api/Cenarios/', CenariosViews.as_view(), name='get_Cenarios'),
-    path('api/Cenario/<uuid:idKey>', CenarioViews.as_view(), name='get_Cenario'),
+    path('api/cenarios', CenariosViews.as_view(), name='get_Cenarios'),
+    path('api/cenario/<uuid:idKey>', CenarioViews.as_view(), name='get_Cenario'),
    
 ]
