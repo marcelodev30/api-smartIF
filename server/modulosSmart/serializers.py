42fa3f0c8bc691f194089595cee784e169d47df5
Marcelo Alves <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-11-26 21:14:28 -0300

    ....

--- server/modulosSmart/serializers.py
+++ server/modulosSmart/serializers.py
@@ -14,13 +14,28 @@ class SalaSerializer(serializers.ModelSerializer):
         model = Sala
         fields = ['nome','local']
 
-class ModeloDisposivitoSerializer(serializers.ModelSerializer):
+class ModeloDisposivitosSerializer(serializers.ModelSerializer):
     class Meta:
         model = ModeloDisposivito
         fields = ['nome','marca','modelo','min_temperatura','max_temperatura']
+    
+class ModeloDisposivitoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModeloDisposivito
+        fields = ['id','nome','marca','modelo','min_temperatura','max_temperatura']
+    
+    def create(self, validated_data):
+        modeloDisposivito = ModeloDisposivito.objects.create(
+            nome=validated_data['nome'],
+            marca=validated_data['marca'],
+            modelo=validated_data['modelo'],
+            min_temperatura=validated_data['min_temperatura'],
+            max_temperatura=validated_data['max_temperatura'],
+        )
+        return modeloDisposivito
 
 class DispositivosSerializer(serializers.ModelSerializer):
-    modelo=ModeloDisposivitoSerializer()
+    modelo=ModeloDisposivitosSerializer()
     sala = SalaSerializer()
     class Meta:
         model = Dispositivos
